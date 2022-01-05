3a9fc3420442cf2c8975a8b595e573a44dae058a
Robbe Brandse <robbe.brandse@gmail.com>
2022-01-05 15:55:24 +0100

    checkmate in x function
    Function in board_utility that can find checkmate in x moves

--- project/Custom_Agent/Board_utility.py
+++ project/Custom_Agent/Board_utility.py
@@ -42,6 +42,37 @@ class BoardUtility(Utility):
                 current_tile += 1
         return board_array
 
+    # (onvermeidelijke) schaakmat vinden binnen x zetten
+    @staticmethod
+    def mate_in_x(board: chess.Board, move_count, depth = 0):
+        for move in list(board.legal_moves):
+            board.push(move)
+            if board.is_checkmate():
+                board.pop()
+                return move
+            # schaakmat is nog niet gevonden
+            # alle mogelijke zetten van de tegenstander overlopen
+            # als voor elke zet een schaakmat mogelijk is, dan werkt het algoritme wel
+            elif move_count > depth + 1: # mogen nog dieper gaan
+                mate_possible = True
+                for opponents_move in list(board.legal_moves):
+                    board.push(opponents_move)
+                    # dieper naar chekmate zoeken
+                    new_move = BoardUtility.mate_in_x(board, move_count, depth+1)
+                    board.pop()
+                    # als geen chekmate gevonden is voor deze zet, dan hebben we geen garantie op schaakmat voor deze diepte
+                    if new_move is None:
+                        # board.pop()
+                        mate_possible = False
+                        break
+                # als de volledige movelist volledig overlopen wordt betekent dit dat alle zetten kunnen resulteren in schaakmat, dus deze zet is ok
+                if mate_possible:
+                    board.pop()
+                    return move
+            board.pop()
+        return None
+
+
     # hier wordt board info door het neural network gehaald om de utility te berekenen
     def board_value(self, board: chess.Board):
         pass
---
+++ project/Custom_Agent/testcode.py
@@ -0,0 +1,27 @@
+import chess
+
+from project.Custom_Agent.Board_utility import BoardUtility
+
+# checkmate in one test
+# board = chess.Board("r1bqkbnr/ppp2ppp/2np4/4p3/2B1P3/5Q2/PPPP1PPP/RNB1K1NR w KQkq - 0 4")
+# best_move = BoardUtility.mate_in_x(board, 1)
+# board.push(best_move)
+# print(board)
+
+
+# checkmate in x test
+board = chess.Board("8/8/1k6/5R2/6R1/8/8/8 w - - 2 2")
+x = 3
+print(board)
+for n in range(x):
+    best_move = BoardUtility.mate_in_x(board, x-n)
+    print(best_move)
+    board.push(best_move)
+    # print(board)
+    if board.is_checkmate():
+        break
+
+    # move for black
+    board.push(list(board.legal_moves)[0])
+print()
+print(board)
